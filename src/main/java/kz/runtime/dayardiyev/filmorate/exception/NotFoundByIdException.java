758fdc1d7fa8ca4792041f9b07a120e6f536ec99
= <=>
2023-11-01 13:44:59 +0600

    ТЗ 9

--- src/main/java/kz/runtime/dayardiyev/filmorate/exception/NotFoundByIdException.java
+++ src/main/java/kz/runtime/dayardiyev/filmorate/exception/NotFoundByIdException.java
@@ -1,6 +1,6 @@
 package kz.runtime.dayardiyev.filmorate.exception;
 
-public class NotFoundByIdException extends RuntimeException{
+public class NotFoundByIdException extends RuntimeException {
     public NotFoundByIdException(String message) {
         super(message);
     }
